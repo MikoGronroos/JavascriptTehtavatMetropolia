b52286cba88cecd2ced4c405b4d67876a9586fec
Miko Gronroos <miko.gronroos@gmail.com>
2025-11-17 18:41:50 +0000

    Add modules

---
+++ Module3/t1/1.js
@@ -0,0 +1,3 @@
+let html = document.getElementById("target")
+html.innerHTML = "<li>First item</li><li>Second item</li><li>Third item</li>" 
+html.classList.add("my-list");
---
+++ Module3/t2/2.js
@@ -0,0 +1,3 @@
+const currentDiv = document.getElementById("");
+let element = createElement("div")
+element.appendChild("<li>First item</li><li>Second item</li><li>Third item</li>")
