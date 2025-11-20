29ec050ab39c124353413fd066551046b3e4a490
Miko Gronroos <miko.gronroos@gmail.com>
2025-11-20 19:14:39 +0000

    Add module 2 teht 1

---
+++ Module2/Tehtava1.js
@@ -0,0 +1,12 @@
+let numbers = [];
+numbers.push(Number(prompt('First number')));
+numbers.push(Number(prompt('Second number')));
+numbers.push(Number(prompt('Third number')));
+numbers.push(Number(prompt('Fourth number')));
+numbers.push(Number(prompt('Fifth number')));
+
+for(let i = numbers.length; i >= 0; i--){
+
+  console.log(numbers[i]);
+
+}
